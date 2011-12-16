cdfaf707079d689eec02b6463872d6b5c0e8aad4
Ludovic Fleury <ludo.fleury@gmail.com>
2011-12-16 03:17:16 +0100

    Fix checkstyle in DepedencyInjection/Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -10,7 +10,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
- 
+
 namespace Plemi\Bundle\PayboxBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
